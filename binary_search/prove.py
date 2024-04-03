218d2057e17a2fbec0d557e7eb1eefc9a0254a47
yousshim <yousef98ibrahim@gmail.com>
2024-04-03 05:54:44 -0400

    refactor: organize file structure
    move binary search implementaion and proof to a searate directory
    to make room for other proofs

--- binary_search/prove.py
+++ binary_search/prove.py
@@ -11,7 +11,9 @@ def sorted(arr: FuncDeclRef) -> BoolRef | QuantifierRef:
     return ForAll([i, j], Implies(i > j, arr(i) >= arr(j)))
 
 
-def in_range(arr: FuncDeclRef, target: ArithRef, l: BitVecRef, r: BitVecRef) -> BoolRef | QuantifierRef:
+def in_range(
+    arr: FuncDeclRef, target: ArithRef, l: BitVecRef, r: BitVecRef
+) -> BoolRef | QuantifierRef:
     i = FreshBitVec("i", 32)
     return Exists(i, And(arr(i) == target, i >= l, i < r))
 
@@ -25,23 +27,31 @@ l, r = BitVecs("l r", 32)
 target = Int("target")
 arr = Function("arr", BitVecSort(32), IntSort())
 
-preconditions = And([
-    l >= 0,
-    r > l, # assume the start range is not empty
-    sorted(arr), # assum the arris is sorted
-    in_range(arr, target, l, r), # assume target exists in start range
-])
+preconditions = And(
+    [
+        l >= 0,
+        r > l,  # assume the start range is not empty
+        sorted(arr),  # assum the arris is sorted
+        in_range(arr, target, l, r),  # assume target exists in start range
+    ]
+)
 
 mid = l + (r - l) / 2
-termination = And([
-    (r - l) > (mid - l),
-    (r - l) > (r - (mid + 1)),
-])
+termination = And(
+    [
+        (r - l) > (mid - l),
+        (r - l) > (r - (mid + 1)),
+    ]
+)
 
-loop_invariant = And([
-    Implies(target < arr(mid), in_range(arr, target, l, mid)),
-    Implies(target > arr(mid), in_range(arr, target, mid + 1, r)),
-])
+# prove that the target can always be found between l and r
+# regardless of which code branch gets executed
+loop_invariant = And(
+    [
+        Implies(target < arr(mid), in_range(arr, target, l, mid)),
+        Implies(target > arr(mid), in_range(arr, target, mid + 1, r)),
+    ]
+)
 
 claim = Implies(preconditions, termination)
 
@@ -50,4 +60,3 @@ prove(claim)
 claim = Implies(preconditions, loop_invariant)
 
 prove(claim)
-

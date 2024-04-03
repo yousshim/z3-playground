40ca3ffbe124f5f331ee39942e9d52c0e042425b
yousshim <yousef98ibrahim@gmail.com>
2024-04-03 04:34:41 -0400

    fix overflow with fixed sized integers
    calculating mid by summing l and r and dividing by 2 can cause an
    error with fixed sized integers if l and r are large enough for
    the addition to overflow. this can be worked around by using the
    formula l + (r - l) / 2 to calculate mid instead. this overflow
    behaviour can be modeled in z3 using BitVec for the two pointers
    instead of Int with is an arbitrary size integer.

--- prove.py
+++ prove.py
@@ -6,14 +6,14 @@ from z3 import *
 # is always decreasing regardless of the
 # executed branch in the loop
 
-l, r = Ints("l r")
+l, r = BitVecs("l r", 32)
 
 preconditions = And([
     l >= 0,
     r > l, # assume the range is not empty
 ])
 
-mid = (l + r) / 2
+mid = l + (r - l) / 2
 termination = And([
     (r - l) > (mid - l),
     (r - l) > (r - (mid + 1)),
